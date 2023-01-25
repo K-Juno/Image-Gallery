a4af69f6c76446fcc1c0eba932305d60214de144
Juno <juno1803@gmail.com>
2023-01-25 21:56:27 +0100

    add comments in upload section

--- components/preview.js
+++ components/preview.js
@@ -1,14 +1,18 @@
-import Image from 'next/image'
-import { useMemo } from 'react'
-import styles from '../styles/Home.module.css'
+import Image from "next/image";
+import { useMemo } from "react";
+import styles from "../styles/Home.module.css";
 
-export default function ImagePreview({
-  file
-}) {
+export default function ImagePreview({ file }) {
   const src = useMemo(() => URL.createObjectURL(file), [file]);
   return (
     <div>
-      <Image src={src} alt="" width="200" height="200" className={styles.images}/>
+      <Image
+        src={src}
+        alt=""
+        width="200"
+        height="200"
+        className={styles.images}
+      />
     </div>
   );
 }
